670d4cefda2fa3e9d4b1c1f5e1d69a021c354c89
Fidel Rojas <>
2022-11-17 14:39:30 +0000

    Function name and what we learned.

---
+++ .ccls-cache/@home@runner@Git-Clone/211-f22-collab-github@collab.c
@@ -0,0 +1,35 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void froja2_pkeit2() {
+  printf("Fidel Rojas and Philip Keith\n");
+  printf("We learned to cd into the repository after cloning and before running "
+      "commands. To check the repositories and files in the program using ls will "
+      "show the repository in the files. To modify a file in the repository using "
+      " git add file.c will update the changes, and using git status will show "
+      "any changes in files, new, and removed files.\n");
+}
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    froja2_pkeit2();
+    
+    return 0;
+}
